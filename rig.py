261696f940023cff52fb74691afcf7957bc71180
simmy <simone.pericic@gmail.com>
2025-10-25 16:11:36 +1030

    changes to rig.py
    1. Fixes to function repair.
    
    2. Created calculate max storage and damge max.
    
    3. Fixed upgrade function.
    
    4. Started fix for damage hit (still needs more work).
    
    5.Created generate assets function using random.choice.

--- rig.py
+++ rig.py
@@ -37,12 +37,8 @@ class Rig:
         return self.__damage
 
     def condition(self):
-        if self.__status is False:
-            print(f'{self.__name} Pristine (Level {self.__damage_max}.')
-        elif self.__status is True and self.__damage is not == self.__damage_max:
-            print
-
-            print(f'{self.__name} Broken (Level 0).')
+        # returns True if rig is broken
+        return self.__condition
 
     def get_storage(self):
         return self.__storage
@@ -58,47 +54,85 @@ class Rig:
     def get_max_storage(self):
         return self.__max_storage
 
-    def calculate_damage_max(self):
-        self.__damage_max = self.__STARTING_DAMAGE +(self.__upgrade_level * self.__STORAGE_PER_LEVEL)
-
-    def __calculate_max_storage(self):
+    def __calculate_storage_max(self):
         self.__max_storage = self.__STARTING_STORAGE + (self.__upgrade_level * self.__ADDITION_DAMAGE_LEVELUP)
+
+    def __find_asset_by_name(self):
+        for asset in self.__storage:
+            if asset.get_name() == name:
+                return asset
+        return None
 
     def get_damage_max(self):
         return self.__damage_max
 
+    def __calculate_damage_max(self):
+        self.__damage_max = self.__STARTING_DAMAGE +(self.__upgrade_level * self.__STORAGE_PER_LEVEL)
+
     def repair(self, crypto_token):
-        if crypto_token.name != 'CryptoToken':
-            raise ValueError('Repair requires a CryptoToken.')
+        if crypto_token.name is None:
+            print(f'Repair requires a CryptoToken to repair {self.__name}.')
+            return False
         elif self.__damage == 0 and not self.__status:
             print(f'{self.__name} is not damaged, no repairs needed.')
+            return False
         else:
             self.__damage =0
             self.__status = False
             print(f'{self.__name} has been repaired.')
+            return True
 
     def upgrade(self, hardware_patch):
-        if hardware_patch.name != 'Hardware Patch':
-            raise ValueError(f'Upgrade requires a Hardware Patch.')
+        if hardware_patch.name is None:
+            print(f'Upgrade requires a Hardware Patch.')
+            return False
         else:
             self.__upgrade_level += 1
-            self.__max_storage += 1
-            self.__damage_max =
+            self.__calculate_storage_max()
+            self.__calculate_damage_max()
             print(f'{self.__name} upgraded to level {self.__upgrade_level}.')
+            print(f'Storage: {self.__max_storage}.'
+                  f'\n Damage Maximum: {self.__damage_max}.')
+            return True
 
     def damage_hit(self):
-        if not self.__status:
+        if self.__condition:
+            print(f'{self.__name} is already broken.')
+            return False
+        else:
             self.__damage += 1
-            if self.__damage >= self.__damage_max:
-                self.__status = True
-            else:
-                print(f'{self.__name} took a hit, damage: {self.__damage}.')
+            print(f'{self.__name} took a hit, Damage: {self.__damage} / {self.__damage_max}.')
+
+        if self.__damage >= self.__damage_max:
+            self.__condition = True
+            print(f'{self.__name} is now broken.'
+                  '\nUnencrypted assets can now be stolen.')
+            return True
+        else:
+            print(f'{self.__name} is still operational.'
+                  f'\n Damage: {self.__damage} / {self.__damage_max}')
+            return False
 
     def __generate_assets(self):
-        #TODO: Generate random assests over time.
+        if len(self.__storage) >= self.__max_storage:
+            print(f'{self.__name} storage is full ({self.__max_storage} / {self.__max_storage}).')
+            print('Can\'t generate any more new assets.')
+            return None
 
-    def __starting_assets(self, crypto_token):
-        #TODO: one crypto token as starting asset
+        assets_gen = [
+            create_crypto_token,
+            create_data_spike,
+            create_removable_drive,
+            create_security_chip,
+            create_hardware_patch
+        ]
+
+        asset = random.choice(assets_gen)()
+        self.__storage.append(asset)
+        print(f'{self.__name} generated: {asset.get.name()}.')
+        print(f'Storage: {len(self.__storage)} / {self.__max_storage}.')
+        return asset
+
 
     def store_assets(self, asset):
         if len(self.__storage) >= self.__max_storage:
@@ -116,6 +150,14 @@ class Rig:
         condition = 'Broken'if self.__status else f'Damage {self.__damage}/{self.__damage_max}'
         return (f'Rig: {self.__name}, Condition {condition},'
                 f'Level: {self.__upgrade_level}, Storage: {}')
+
+    if self.__status is False:
+        print(f'{self.__name} Pristine (Level {self.__damage_max}.')
+    elif self.__status is True and self.__damage is not == self.__damage_max:
+        print
+
+        print(f'{self.__name} Broken (Level 0).')
+
 
     name = property(get_name, set_name)
     damage = property(get_damage)
